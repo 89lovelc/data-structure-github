901efee565e28954d15aacd9c903361e153f21dd
huangjh5 <huangjh5@asiainfo.com>
2019-10-08 20:11:19 +0800

    add:初始化队列迷宫

---
+++ _003_queue_maze/src/main/java/com/lovelc/Node.java
@@ -0,0 +1,27 @@
+package com.lovelc;
+
+import java.util.Objects;
+
+public class Node {
+
+    public int x;
+
+    public int y;
+
+    public Node pre;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Node node = (Node) o;
+        return x == node.x &&
+                y == node.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
